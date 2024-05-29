e1b086e52a28639cdfb900666c11c086352155be
BIPIN SAHANI <bipinsahani786@gmail.com>
2024-05-29 23:55:07 +0530

    authentication login signup firebase

--- public/js/form.js
+++ public/js/form.js
@@ -1,77 +1,97 @@
-const loader = document.querySelector('.loader');
-
-const submitBtn = document.querySelector('.submit-btn');
-const name = document.querySelector('#name');
-const email = document.querySelector('#email');
-const password = document.querySelector('#password');
-const number = document.querySelector('#number');
-const tac = document.querySelector('#terms-and-condition');
-const notification = document.querySelector('#notification');
-
-submitBtn.addEventListener('click', () => {
-    if(name.value.length < 3){
-         showAlert('name must be 3 letters long');
+//redirect to home page if user logged in
+window.onload = () => {
+  if (sessionStorage.user) {
+    user = JSON.parse(sessionStorage.user);
+    if (compareToken(user.authToken, user.email)) {
+      location.replace("/");
     }
-    else if(!email.value.length){
-        showAlert('enter your email');
+  }
+};
 
-    }
-    else if(password.value.length <8){
-        showAlert('password should be 8 letters long');
-    } 
-    else if(!number.value.length){
-        showAlert("enter your phone number");
-    }
-    else if(!Number(number.value) || number.value.length < 10){
-       showAlert("invalid number, please enter valid one");
+const loader = document.querySelector(".loader");
 
-    }
-    else if(!tac.checked){
-        showAlert("you must agree to our terms and condition ")
-    }
-    else{
-        // submit form 
-        loader.style.display = 'block';
-        sendData('/signup', {
-            name:name.value,
-            email:email.value,
-            password:password.value,
-            number:number.value,
-            tac:tac.checked,
-            notification:notification.checked,
-            seller:false
+const submitBtn = document.querySelector(".submit-btn");
+const name = document.querySelector("#name") || null;
+const email = document.querySelector("#email");
+const password = document.querySelector("#password");
+const number = document.querySelector("#number") || null;
+const tac = document.querySelector("#terms-and-condition") || null;
+const notification = document.querySelector("#notification") || null;
 
-        })
+submitBtn.addEventListener("click", () => {
+  if (name != null) {
+    if (name.value.length < 3) {
+      showAlert("name must be 3 letters long");
+    } else if (!email.value.length) {
+      showAlert("enter your email");
+    } else if (password.value.length < 8) {
+      showAlert("password should be 8 letters long");
+    } else if (!number.value.length) {
+      showAlert("enter your phone number");
+    } else if (!Number(number.value) || number.value.length < 10) {
+      showAlert("invalid number, please enter valid one");
+    } else if (!tac.checked) {
+      showAlert("you must agree to our terms and condition ");
+    } else {
+      // submit form
+      loader.style.display = "block";
+      sendData("/signup", {
+        name: name.value,
+        email: email.value,
+        password: password.value,
+        number: number.value,
+        tac: tac.checked,
+        notification: notification.checked,
+        seller: false,
+      });
     }
+  } else {
+    //login page
+    if (!email.value.length || !password.value.length) {
+      showAlert("fill all the details");
+    } else {
+      loader.style.display = "block";
+      sendData("/login", {
+        email: email.value,
+        password: password.value,
+      });
+    }
+  }
+});
 
-})
-
-//send data function
 const sendData = (path, data) => {
-fetch(path, {
-    method:'post',
-    headers: new Headers({'Content-Type': 'application/json'}),
-    body:JSON.stringify(data)
-}).then((res) => res.json())
-.then(response => {
-    // console.log(response);
-    processData(response);
-})
-}
-const processData = (data) => {
+    fetch(path, {
+      method: "post",
+      headers: new Headers({ "Content-Type": "application/json" }),
+      body: JSON.stringify(data),
+    })
+      .then((res) => res.json())
+      .then((response) => {
+        console.log(response);
+        processData(response);
+      });
+  };
+  const processData = (data) => {
     loader.style.display = null;
-    if(data.alert){
-        showAlert(data.alert);
+    if (data.alert) {
+      showAlert(data.alert);
+    } else if (data.name) {
+      // console.log(data)
+      //create auth token
+      data.authToken = generateToken(data.email);
+      sessionStorage.user = JSON.stringify(data);
+      location.replace("/");
     }
-}
-
-// alert function 
-const showAlert = (msg) => {
-    let alertBox = document.querySelector('.alert-box');
-    let alertMsg = document.querySelector('.alert-msg');
+  };
+  
+  // alert function
+  const showAlert = (msg) => {
+    let alertBox = document.querySelector(".alert-box");
+    let alertMsg = document.querySelector(".alert-msg");
     alertMsg.innerHTML = msg;
-    alertBox.classList.add('show');
+    alertBox.classList.add("show");
     setTimeout(() => {
-        alertBox.classList.remove('show');
+      alertBox.classList.remove("show");
     }, 2000);
-}
+  };
+  
--- server.js
+++ server.js
@@ -1,8 +1,19 @@
 //importing packages
-const express = require('express')
-const admin = require('firebase-admin')
-const bcrypt = require('bcrypt')
-const path = require('path')
+const express = require("express");
+const admin = require("firebase-admin");
+const bcrypt = require("bcrypt");
+const path = require("path");
+
+//firebase setup
+
+let serviceAccount = require("./ecommerce-a06ac-firebase-adminsdk-rspc3-fa9cdc65c5.json");
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+});
+
+let db = admin.firestore();
+
 //declare static path
 let staticPath = path.join(__dirname, "public");
 //initializing express.js
@@ -13,51 +24,99 @@ app.use(express.static(staticPath));
 app.use(express.json());
 //routes
 //home route
-app.get("/" , (req, res) => {
-    res.sendFile(path.join(staticPath , "index.html"));
-})
+app.get("/", (req, res) => {
+  res.sendFile(path.join(staticPath, "index.html"));
+});
 //signup route
-app.get('/signup', (req, res) => {
-    res.sendFile(path.join(staticPath, "signup.html"));
-})
+app.get("/signup", (req, res) => {
+  res.sendFile(path.join(staticPath, "signup.html"));
+});
 
-app.post('/signup',(req, res) => {
-    // console.log(req.body);
-    let {name, email, password, number, tac, notification} = req.body;
-    //form validations
-    if(name.length < 3){
-        return res.json({'alert':'name must be 3 letters long'});
-   }
-   else if(!email.length){
-       return res.json({'alert':'enter your email'});
+app.post("/signup", (req, res) => {
+  console.log(req.body);
+  let { name, email, password, number, tac, notification } = req.body;
+  //form validations
+  if (name.length < 3) {
+    return res.json({ alert: "name must be 3 letters long" });
+  } else if (!email.length) {
+    return res.json({ alert: "enter your email" });
+  } else if (password.length < 8) {
+    return res.json({ alert: "password should be 8 letters long" });
+  } else if (!number.length) {
+    return res.json({ alert: "enter your phone number" });
+  } else if (!Number(number) || number.length < 10) {
+    return res.json({ alert: "invalid number, please enter valid one" });
+  } else if (!tac) {
+    return res.json({ alert: "you must agree to our terms and condition" });
+  }
 
-   }
-   else if(password.length <8){
-       return res.json({'alert':'password should be 8 letters long'});
-   } 
-   else if(!number.length){
-       return res.json({'alert':'enter your phone number'});
-   }
-   else if(!Number(number) || number.value.length < 10){
-      return res.json({'alert':'invalid number, please enter valid one'})
-   }
-   else if(!tac.checked){
-    return res.json({'alert': 'you must agree to our terms and condition'});
-   }
-   else {
-    //store user in db
-   }
-    res.json('data received')
+  //store user in db
+  db.collection("users")
+    .doc(email)
+    .get()
+    .then((user) => {
+      if (user.exists) {
+        return res.json({ alert: "email already exists" });
+      } else {
+        //encrypt the password before storing it
+        bcrypt.genSalt(10, (err, salt) => {
+          bcrypt.hash(password, salt, (err, hash) => {
+            req.body.password = hash;
+            db.collection("users")
+              .doc(email)
+              .set(req.body)
+              .then((data) => {
+                res.json({
+                  name: req.body.name,
+                  email: req.body.email,
+                  seller:req.body.seller,
+                });
+              });
+          });
+        });
+      }
+    });
+
+ 
+});
+//login route
+app.get('/login',(req, res) => {
+  res.sendFile(path.join(staticPath, "login.html"));
 } )
 
-//404 route
-app.use('/404',(req, res) => {
-    res.sendFile(path.join(staticPath, "404.html" ))
+app.post('/login', (req, res) => {
+  let {email, password} = req.body;
+  if(!email.length || !password.length){
+    return res.json({'alert': 'fill all the inputs'})
+  }
+  db.collection('users').doc(email).get()
+  .then(user => {
+    if(!user.exists){
+      return res.json({'alert': 'log in email does not exists'})
+    }else{
+      bcrypt.compare(password, user.data().password, (err, result) => {
+        if(result){
+          let data = user.data();
+          return res.json({
+            name: data.name,
+            email: data.email,
+          seller: data.seller,
+          })
+        }else{
+          return res.json({'alert' : 'password is incorrect'});
+        }
+      })
+    }
+  })
 })
+//404 route
+app.use("/404", (req, res) => {
+  res.sendFile(path.join(staticPath, "404.html"));
+});
 
 app.use((req, res) => {
-    res.redirect('/404');
-})
+  res.redirect("/404");
+});
 app.listen(3000, () => {
-    console.log("listening on port 3000..........");
-})
+  console.log("listening on port 3000..........");
+});
